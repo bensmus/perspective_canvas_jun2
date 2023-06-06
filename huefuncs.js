193dabfae294301c738787073ebd789d4ac7087d
Ben Smus <bensmus@gmail.com>
2023-06-05 23:30:59 -0700

    neater

--- huefuncs.js
+++ huefuncs.js
@@ -1,4 +1,16 @@
+// -------- all huefuncs  ---------- //
 const huefuncs = [colorshift, onlyred]
+
+function colorshift(x, y, z, t) {
+    const dist_frac = vector_distance(x, y, z) / max_distance
+    return ((triangle_wave(t / 30000) + dist_frac) / 2) ** 2
+}
+
+function onlyred(x, y, z, t) {
+    return 0
+}
+// --------------------------------- //
+
 let huefuncs_index = 0
 const max_distance = Math.sqrt(3)
 
@@ -20,13 +32,4 @@ function frac(x) {
 
 function triangle_wave(x) {
     return Math.abs(2 * frac(x) - 1)
-}
-
-function colorshift(x, y, z, t) {
-    const dist_frac = vector_distance(x, y, z) / max_distance
-    return ((triangle_wave(t / 30000) + dist_frac) / 2) ** 2
-}
-
-function onlyred(x, y, z, t) {
-    return 0
 }
