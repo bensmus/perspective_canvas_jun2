0e28ec83110c5df2453869b6b7e0ca9b4f1ba3aa
Ben Smus <bensmus@gmail.com>
2023-06-09 13:18:55 -0700

    about to make info panel work

---
+++ info_gui.js
@@ -0,0 +1,10 @@
+const info_open = document.getElementById('info_open')
+const info_close = document.getElementById('info_close')
+const info_text = document.getElementById('info_text')
+
+// info gui is visible by default
+let open = true
+
+info_close.addEventListener('click', () => {
+
+})
