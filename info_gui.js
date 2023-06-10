a2aae88c1a5b958794e3117441427ce20eead172
Ben Smus <bensmus@gmail.com>
2023-06-09 19:01:11 -0700

    info gui works

--- info_gui.js
+++ info_gui.js
@@ -2,9 +2,16 @@ const info_open = document.getElementById('info_open')
 const info_close = document.getElementById('info_close')
 const info_text = document.getElementById('info_text')
 
-// info gui is visible by default
-let open = true
-
 info_close.addEventListener('click', () => {
+    console.log('closing')
+    info_close.style.display = 'none'
+    info_text.style.display = 'none'
+    info_open.style.display = 'block'
+})
 
+info_open.addEventListener('click', () => {
+    console.log('opening')
+    info_open.style.display = 'none'
+    info_close.style.display = 'block'
+    info_text.style.display = 'block'
 })
